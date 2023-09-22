1c0fe1c707c27f28a033f7408d28a07072a2958d
sjoleee <cent7425@gmail.com>
2023-09-22 20:00:21 +0900

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import ErrorBoundary from "./components/ErrorBoundary.tsx";
 
 const queryClient = new QueryClient();
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
