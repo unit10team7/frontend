9edc29c073c1efe9fdf9fa2c0ab8c7c747e692a7
sjoleee <cent7425@gmail.com>
2023-09-23 19:57:31 +0900

    feat: useStep 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import CTAButton from "./components/CTAButton";
 import Camera from "./components/Camera";
+import CameraStep from "./components/steps/CameraStep";
+import CompleteStep from "./components/steps/CompleteStep";
+import FrameSelectStep from "./components/steps/FrameSelectStep";
+import PhotoSelectStep from "./components/steps/PhotoSelectStep";
+import useStep from "./hooks/useStep";
+
+const elements = [
+  <PhotoSelectStep key="PhotoSelectStep" />,
+  <FrameSelectStep key="FrameSelectStep" />,
+  <CameraStep key="CameraStep" />,
+  <CompleteStep key="CompleteStep" />,
+];
 
 function App() {
+  const { currentElement, currentStep, moveBackward, moveForward } = useStep({
+    elements: elements,
+  });
+
   return (
     <main>
-      <Camera />
+      {currentElement}
+      {elements.length - 1 > currentStep && (
+        <CTAButton onClick={() => moveForward()}>다음스텝으로</CTAButton>
+      )}
     </main>
   );
 }
---
+++ src/components/steps/PhotoSelectStep.tsx
@@ -0,0 +1,5 @@
+const PhotoSelectStep = () => {
+  return <div>첫번째 스텝, 사진 4장 고르는 스텝입니다.</div>;
+};
+
+export default PhotoSelectStep;
