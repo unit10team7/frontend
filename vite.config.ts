4f64df3e64aad3f8acb4ba345ab86b7112d266ba
sjoleee <cent7425@gmail.com>
2023-09-24 09:28:58 +0900

    hotfix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/frontend/",
-  optimizeDeps: {
-    exclude: ["embla-carousel-react"],
-  },
 });
